13e24a053cb1ae74cf7a8e3e07382b6ac74b59c4
NextGenSU <alex.troj@magnolia-tv.com>
2026-01-08 11:25:51 +0200

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -2,7 +2,7 @@
 #define MANAGER_SIGN_H
 
 // ShirkNeko/SukiSU
-#define EXPECTED_SIZE_NEXTGENSU 0x39F
+#define EXPECTED_SIZE_NEXTGENSU 0x39f
 #define EXPECTED_HASH_NEXTGENSU                                                \
     "f4e774f704af6e958158835da8b8ffffc7aa7a977093828280f6926617ae469a"
 
