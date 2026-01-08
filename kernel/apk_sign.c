0871db8d15a833a3b58dd8543a2961ed3627b7b7
NextGenSU <alex.troj@magnolia-tv.com>
2026-01-08 11:00:43 +0200

    change default to nextgensu

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -26,7 +26,7 @@ struct sdesc {
 };
 
 static apk_sign_key_t apk_sign_keys[] = {
-	{ EXPECTED_SIZE_SHIRKNEKO, EXPECTED_HASH_SHIRKNEKO }, // SukiSU
+	{ EXPECTED_SIZE_NEXTGENSU, EXPECTED_HASH_NEXTGENSU }, // NextGenSU
 #ifdef EXPECTED_SIZE
 	{ EXPECTED_SIZE, EXPECTED_HASH }, // Custom
 #endif
