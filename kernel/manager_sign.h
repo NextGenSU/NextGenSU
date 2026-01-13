6744deb1b9d7aefaf5cd52768be701e4ace5b071
NextGenSU <alex.troj@magnolia-tv.com>
2026-01-13 20:48:17 +0200

    nextgensu

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -4,8 +4,8 @@
 #include <linux/types.h>
 
 // NextGenSU/KernelSU
-#define EXPECTED_SIZE_SHIRKNEKO 0x39f
-#define EXPECTED_HASH_SHIRKNEKO                                                \
+#define EXPECTED_SIZE_NEXTGENSU 0x39f
+#define EXPECTED_HASH_NEXTGENSU                                                \
 	"f4e774f704af6e958158835da8b8ffffc7aa7a977093828280f6926617ae469a"
 
 // rsuntk/KernelSU
